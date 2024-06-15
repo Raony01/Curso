7e8ff8515215421d42a31b111012e3f8ec3ffbdb
Raony01 <raonyaprigio234@gmail.com>
2024-06-15 18:37:43 -0300

    initial commit

---
+++ exercicio3.js
@@ -0,0 +1,16 @@
+var n1, n2, n3, n4, media
+
+n1 = 6;
+n2 = 6;
+n3 = 6;
+n4 = 6;
+
+media = (n1 + n2 + n3 + n4) / 4;
+
+console.log("Sua media final foi: " + media)
+
+if(media >= 7){
+    console.log("Bom dia Gabriel, você foi aprovado");
+}else{
+    console.log("Bom dia Gabriel, infelizmente você foi Reprovado");
+};
